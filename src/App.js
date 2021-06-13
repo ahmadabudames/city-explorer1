6960152ee7eaf1320cddb012f6336d2f34bf0e17
ahmadabudames <ahmadabudamesmais1999@gmail.com>
2021-06-13 21:03:11 +0300

    lab06

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export class App extends Component {
+
+
+
+ 
+  constructor(props){
+    super(props);
+    this.state={
+      mycityName : ''
+    }
+  }
+ 
+ 
+   updateCityNameState= (e) =>{
+     this.setState({
+      mycityName :e.target.value 
+       
+     })
+     console.log(this.state)
+   }
+
+
+  render() {
+    return (
+      <div>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Enter City Name</Form.Label>
+            <Form.Control onChange={this.updateCityNameState} type="text" placeholder="Enter City Name" />
+
+
+
+
+
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Explore
+          </Button>
+        </Form>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
