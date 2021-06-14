9b32d0c600767c80b01e81d71689afae21360c47
ahmadabudames <ahmadabudamesmais1999@gmail.com>
2021-06-14 22:19:43 +0300

    finished lab6

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-
+import Image from 'react-bootstrap/Image'
 
 export class Map extends Component {
     render() {
         return (
             <div>
-                 <img src={`https:maps.locationiq.com/v3/staticmap?key=pk.4cbbc812278b30d2b7dbaedbcc824622&q¢er=${this.props.cityData.lat},${this.props.cityData.lon}&zoom=15`}/>
+                <Image src={`https://maps.locationiq.com/v3/staticmap?key=pk.4cbbc812278b30d2b7dbaedbcc824622&q&center=${this.props.cityData.lat},${this.props.cityData.lon}&zoom=15`} rounded />
             </div>
         )
     }
