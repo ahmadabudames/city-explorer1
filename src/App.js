2ea1efda8048081e267b4cf9a2febfa62228fa67
ahmadabudames <ahmadabudamesmais1999@gmail.com>
2021-06-14 18:30:52 +0300

    lab06

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import Searchforms from './components/Searchforms';
 import "bootstrap/dist/css/bootstrap.min.css";
-
-
+import axios from 'axios';
+import Alertmessage from './components/Alertmessage';
+import Map from './components/Map';
+import CityData from './components/CityData';
 
 export class App extends Component {
 
@@ -13,37 +14,72 @@ export class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      mycityName : ''
+      mycityName : '',
+      cityData:{},
+      show:false,
+      alert:'',
+      error:''
     }
   }
  
  
    updateCityNameState= (e) =>{
      this.setState({
-      mycityName :e.target.value 
+      mycityName :e.target.value,
        
      })
-     console.log(this.state)
+    //  console.log(this.state)
+   }
+
+   getCityData=async (e)=>{
+     e.preventDefault();
+     try{ const axiosData= await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.4cbbc812278b30d2b7dbaedbcc824622&q=${this.state.mycityName}&format=json`);
+
+     this.setState({
+      cityData:axiosData.data[0],
+      show:true,
+      alert:false
+     })
+    }
+    catch (error){
+      this.setState({
+        error:error.message,
+        alert:true
+      })
+    }
+    
+
+    //  console.log(axiosData);
    }
 
 
   render() {
     return (
       <div>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Enter City Name</Form.Label>
-            <Form.Control onChange={this.updateCityNameState} type="text" placeholder="Enter City Name" />
+         {this.state.alert&&
+       <Alertmessage
+       error={this.state.error}
+       />
+         }
+       <Searchforms
+       getCityData={this.getCityData}
+       updateCityNameState={this.updateCityNameState}
+       />
 
+        {(this.state.show) &&
+        <>
+      
+        <Map
+        cityData={this.state.cityData}
+        />
+    
+     <CityData
+     cityData={this.state.cityData}
+     
+     />
 
-
-
-
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Explore
-          </Button>
-        </Form>
+        </>
+        }
       </div>
     )
   }
---
+++ src/components/Searchforms.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+export class Searchforms extends Component {
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.props.getCityData}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Enter City Name</Form.Label>
+                        <Form.Control onChange={this.props.updateCityNameState} type="text" placeholder="Enter City Name" />
+
+
+
+
+
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Explore
+                    </Button>
+                </Form>
+            </div>
+        )
+    }
+}
+
+export default Searchforms
