35fcfe41cb99495b117956ae5dfe57a88295e4a2
ahmadabudames <ahmadabudamesmais1999@gmail.com>
2021-06-15 16:53:27 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import Alertmessage from './components/Alertmessage';
 import Map from './components/Map';
 import CityData from './components/CityData';
+ import Weather from './components/Weather';
 
 export class App extends Component {
 
@@ -35,12 +36,18 @@ export class App extends Component {
      e.preventDefault();
      try{ const axiosData= await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.4cbbc812278b30d2b7dbaedbcc824622&q=${this.state.mycityName}&format=json`);
 
+     const myApi=await axios.get('http://localhost:8080/weather')
+    //  console.log( myApi)
      this.setState({
+
       cityData:axiosData.data[0],
+      weatherData:myApi.data,
       show:true,
       alert:false
      })
+    
     }
+   
     catch (error){
       this.setState({
         error:error.message,
@@ -76,6 +83,9 @@ export class App extends Component {
      <CityData
      cityData={this.state.cityData}
      
+     />
+     <Weather
+     weather={this.state.weatherData}
      />
 
         </>
